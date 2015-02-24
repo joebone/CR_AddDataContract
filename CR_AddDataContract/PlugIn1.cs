b50d34b1b8cb0d96a1d2c211847dd79df27c6647
joeboneuk@gmail.com <joeboneuk@gmail.com>
2015-02-24 14:05:27 +0100

    VB+c# DataContract/UnDataContract

--- CR_AddDataContract/PlugIn1.cs
+++ CR_AddDataContract/PlugIn1.cs
@@ -1,5 +1,4 @@
-using System;
-using System.ComponentModel;
+//using System.ComponentModel;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -35,8 +34,16 @@ namespace CR_AddDataContract {
             AddDataContract.CheckAvailability += AddDataContract_CheckAvailability;
             AddDataContract.Apply += AddDataContract_Apply;
             ((System.ComponentModel.ISupportInitialize)(AddDataContract)).EndInit();
+
+            DevExpress.CodeRush.Core.CodeProvider RemoveDataContract = new DevExpress.CodeRush.Core.CodeProvider(components);
+            ((System.ComponentModel.ISupportInitialize)(RemoveDataContract)).BeginInit();
+            RemoveDataContract.ProviderName = "RemoveDataContract "; // Should be Unique
+            RemoveDataContract.DisplayName = "Remove DataContract";
+            RemoveDataContract.CheckAvailability += RemoveDataContract_CheckAvailability;
+            RemoveDataContract.Apply += RemoveDataContract_Apply;
+            ((System.ComponentModel.ISupportInitialize)(RemoveDataContract)).EndInit();
         }
-        private void AddDataContract_CheckAvailability(Object sender, CheckContentAvailabilityEventArgs ea) {
+        private void AddDataContract_CheckAvailability(System.Object sender, CheckContentAvailabilityEventArgs ea) {
             // Limit availability to when the caret is within the name of the active class.
             if (CodeRush.Source.ActiveClass == null)
                 return; // No active class
@@ -45,15 +52,38 @@ namespace CR_AddDataContract {
             ea.Available = true;
         }
 
-        private void AddDataContract_Apply(Object sender, ApplyContentEventArgs ea) {
+        #region Language stuff
+        private void ConfigureLanguageConstants(TextDocument activeDoc) {
+            ActiveLanguage = activeDoc.Language;
+            if (activeDoc.Language == "Basic") {
+                TrueConstant = "True";
+                AttributeValueAssignmentOperator = ":=";
+                FalseConstant = "False";
+            }
+            else {
+                TrueConstant = "true";
+                AttributeValueAssignmentOperator = "=";
+                FalseConstant = "false";
+            }
+        }
+        private string AttributeValueAssignmentOperator { get; set; }
+        private string TrueConstant { get; set; }
+        private string FalseConstant { get; set; }
+        private string ActiveLanguage { get; set; }
+        #endregion
+
+        #region Add Attribute
+
+        private void AddDataContract_Apply(System.Object sender, ApplyContentEventArgs ea) {
             TextDocument ActiveDoc = CodeRush.Documents.ActiveTextDocument;
+
+            ConfigureLanguageConstants(ActiveDoc);
 
             using (ActiveDoc.NewCompoundAction("Add DataContract")) {
                 // Add Namespace Reference
                 AddNamespaceReference("System.Runtime.Serialization");
                 CodeRush.Project.AddReference(ActiveDoc.ProjectElement, "System.Runtime.Serialization");
 
-                // Add DataContract Attribute
                 AddAttribute(CodeRush.Source.ActiveClass, "DataContract");
                 int dataOrder = 0;
                 foreach (Property prop in CodeRush.Source.ActiveClass.AllProperties) {
@@ -63,6 +93,8 @@ namespace CR_AddDataContract {
                 CodeRush.Documents.ActiveTextDocument.ApplyQueuedEdits();
                 CodeRush.Documents.ActiveTextDocument.ParseIfNeeded();
                 CodeRush.Actions.Get("FormatFile").DoExecute();
+
+
             }
         }
         private void AddNamespaceReference(string NamespaceName) {
@@ -85,38 +117,150 @@ namespace CR_AddDataContract {
             var Code = CodeRush.CodeMod.GenerateCode(new NamespaceReference(NamespaceName));
             ActiveDoc.QueueInsert(InsertionPoint, Code);
         }
-
         private void AddAttribute(LanguageElement element, string AttributeName, int? DataOrder = null) {
-            var Builder = new ElementBuilder();
+            //var Builder = new ElementBuilder();
+            var Builder = CodeRush.Language.GetElementBuilder(ActiveLanguage); //DevExpress.CodeRush.Common.Constants.Str.Language.CSharp
+
             var Attribute = Builder.BuildAttribute(AttributeName);
 
             if (AttributeName == "DataMember") {
 
                 if (DataOrder.HasValue) {
-                    Attribute.Arguments.Add(
-                    new AssignmentExpression(
-                        new ElementReferenceExpression("Order"), "=", new PrimitiveExpression(DataOrder.ToString())));
+                    Attribute.Arguments.Add(Builder.BuildAssignmentExpression("Order" + AttributeValueAssignmentOperator, DataOrder, AssignmentOperatorType.None));
                 }
+                Attribute.Arguments.Add(Builder.BuildAssignmentExpression("Name" + AttributeValueAssignmentOperator, "\"" + element.Name + "\"", AssignmentOperatorType.None));
+                Attribute.Arguments.Add(Builder.BuildAssignmentExpression("EmitDefaultValue" + AttributeValueAssignmentOperator, FalseConstant, AssignmentOperatorType.None));
+                Attribute.Arguments.Add(Builder.BuildAssignmentExpression("IsRequired" + AttributeValueAssignmentOperator, FalseConstant, AssignmentOperatorType.None));
 
-                Attribute.Arguments.Add(
-                  new AssignmentExpression(
-                      new ElementReferenceExpression("Name"), "=", new PrimitiveExpression("\"" + element.Name + "\"")));
-                Attribute.Arguments.Add(
-                    new AssignmentExpression(
-                        new ElementReferenceExpression("EmitDefaultValue"), "=", new PrimitiveExpression("false")));
-                Attribute.Arguments.Add(
-                   new AssignmentExpression(
-                       new ElementReferenceExpression("IsRequired"), "=", new PrimitiveExpression("false")));
+
+
+                //foreach (var arg in Attribute.Arguments.OfType<AssignmentExpression>().ToList()) {
+                //    // Devexpress too stupid to code attribute assignments correctly in both languages.
+                //    var argg = new AssignmentExpression(arg.LeftSide, AttributeValueAssignmentOperator, arg.RightSide);
+                //    Attribute.Arguments.Remove(arg);
+                //    Attribute.Arguments.Add(argg);
+                //    //new ElementReferenceExpression("IsRequired"), AttributeValueAssignmentOperator, new PrimitiveExpression(FalseConstant)));
+                //    //arg.OperatorText = AttributeValueAssignmentOperator;
+                //    //arg.NewTokens;
+                //}
+
+
+                //AssignmentExpression asign = ;
+                //    new AssignmentExpression(
+                //        new ElementReferenceExpression("EmitDefaultValue"), AttributeAssignmentOperator, new PrimitiveExpression(FalseConstant)));
+                //Attribute.Arguments.Add(
+                //   new AssignmentExpression(
+                //       new ElementReferenceExpression("IsRequired"), AttributeAssignmentOperator, new PrimitiveExpression(FalseConstant)));
 
             }
 
+            var currentElement = element as IHasAttributes;
+            if (currentElement != null) {
+                if (currentElement.Attributes.Count > 0) {
+                    var VSec = currentElement.Attributes[0].Parent as AttributeSection;
+                    var NewVSec = Builder.BuildAttributeSection();
+                    foreach (Attribute att in currentElement.Attributes) {
+                        if (new[] { "DataContract", "DataMember" }.Contains(att.Name))
+                            continue;
+
+                        NewVSec.AddAttribute(att);
+                    }
+                    NewVSec.AddAttribute(Attribute);
+                    var Code = CodeRush.CodeMod.GenerateCode(NewVSec); //var Code = CodeRush.CodeMod.GenerateCode(NewVSec, true);
+                    CodeRush.Documents.ActiveTextDocument.QueueReplace(VSec, Code);
+                }
+                else {
+                    BuildNInsert(element, Builder, Attribute);
+                }
+            }
+            else {
+                BuildNInsert(element, Builder, Attribute);
+            }
+        }
+
+        private static void BuildNInsert(LanguageElement element, ElementBuilder Builder, Attribute Attribute) {
             var Section = Builder.BuildAttributeSection();
             Section.AddAttribute(Attribute);
             var Code = CodeRush.CodeMod.GenerateCode(Section, false);
             SourcePoint InsertionPoint = new SourcePoint(element.Range.Start.Line, 1);
             CodeRush.Documents.ActiveTextDocument.QueueInsert(InsertionPoint, Code);
         }
+        #endregion
+
+        #region Remove Attribute
+
+        private void RemoveDataContract_CheckAvailability(object sender, CheckContentAvailabilityEventArgs ea) {
+            // Limit availability to when the caret is within the name of the active class.
+            if (CodeRush.Source.ActiveClass == null)
+                return; // No active class
+            if (!CodeRush.Source.ActiveClass.NameRange.Contains(CodeRush.Caret.SourcePoint))
+                return;  // Caret not in class name
+            if (!CodeRush.Source.ActiveClass.Attributes.OfType<DevExpress.CodeRush.StructuralParser.Attribute>().Any(att => att.Name == "DataContract"))
+                return;  // Class doesn't implement DataContract
+
+            ea.Available = true;
+        }
+        private void RemoveDataContract_Apply(object sender, ApplyContentEventArgs ea) {
+            TextDocument ActiveDoc = CodeRush.Documents.ActiveTextDocument;
+            ConfigureLanguageConstants(ActiveDoc);
+
+            using (ActiveDoc.NewCompoundAction("Remove DataContract")) {
+
+                RemoveAttribute(CodeRush.Source.ActiveClass, "DataContract");
+                foreach (Property prop in CodeRush.Source.ActiveClass.AllProperties) {
+                    // Add DataMember Attribute
+                    RemoveAttribute(prop, "DataMember");
+                }
+                CodeRush.Documents.ActiveTextDocument.ApplyQueuedEdits();
+                CodeRush.Documents.ActiveTextDocument.ParseIfNeeded();
+                CodeRush.Actions.Get("FormatFile").DoExecute();
 
 
+            }
+        }
+        private void RemoveAttribute(IHasAttributes element, string param1) {
+
+            var att = element.Attributes.Cast<Attribute>().FirstOrDefault(g => g.Name == param1);
+            if (att != null) {
+                if (element.Attributes.Count == 1) {
+                    var ac = att.Parent as AttributeSection;
+                    if (ac != null)
+                        CodeRush.Documents.ActiveTextDocument.QueueDelete(ac);
+                }
+                else {
+                    var ac = att.Parent as AttributeSection;
+                    if (ac != null) {
+                        //ac.RemoveAttribute(att);
+                        var Builder = CodeRush.Language.GetElementBuilder(ActiveLanguage);
+                        var NCC = ac.AttributeCollection.Cast<Attribute>().Except(new[] { att });
+
+                        var Section = Builder.BuildAttributeSection();
+                        foreach (var attL in NCC) Section.AddAttribute(attL);
+                        var Code = CodeRush.CodeMod.GenerateCode(Section, true);
+
+                        CodeRush.Documents.ActiveTextDocument.QueueReplace(ac, Code);
+                    }
+
+
+                    //CodeRush.Documents.ActiveTextDocument.QueueDelete(att);
+                }
+
+
+
+                //att.RemoveAllNodes();
+                //att.RemoveFromParent();
+            }
+
+            //ActiveDoc
+            //activeClass.FindAttribute(param1).RemoveFromParent();
+        }
+
+        //private void RemoveAttribute(IHasAttributes prop, string param1) {
+        //    prop.FindAttribute(param1).RemoveFromParent();
+        //}
+
+
+
+        #endregion
     }
 }
